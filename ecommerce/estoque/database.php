34b4b4750419048a41ada9649119fd7208e4207d
DeividAlmeida <mr.davialmeida@hotmail.com>
2021-07-23 17:33:54 -0300

    identificador

--- ecommerce/estoque/database.php
+++ ecommerce/estoque/database.php
@@ -18,8 +18,21 @@ function ler($i){
 $tabela = [];
 $data = ler("SELECT * FROM ecommerce ORDER BY id DESC");
 if(is_array($data)){
-    foreach($data as $chave => $valor){        
-       
+    foreach($data as $chave => $valor){ 
+        $atributo = ler("SELECT MIN(id_atributo) AS id_atributo FROM ecommerce_prod_termos WHERE id_produto = {$valor['id']} GROUP BY id_atributo");
+        $termo = ler("SELECT * FROM ecommerce_prod_termos WHERE id_produto = {$valor['id']}");
+        $length = count($atributo)*count($atributo);
+        for ($i = 0; $i < $length; $i++) {
+            # $tabela[$chave][$i.'termo'] = $termo[$i]['id_termo']." ".$termo[count($termo)-$i]['id_termo'];
+            if($i<count($atributo)){
+                $tabela[$chave][$i.'termo'] = $termo[$i]['id_termo']." ".$termo[count($atributo)]['id_termo'];
+            }else if($i == count($atributo)){
+                $tabela[$chave][$i.'termo'] = $termo[$i-count($atributo)]['id_termo']." ".$termo[$i+1]['id_termo'];
+            }else{
+                $tabela[$chave][$i.'termo'] = $termo[$i-count($atributo)]['id_termo']." ".$termo[$i]['id_termo'];
+            }
+            
+        }
         $tabela[$chave]['<div class="hidden">'] = ""; 
         $tabela[$chave]['id'] = $valor['id'];
         $tabela[$chave]['imagem'] = "<img src='wa/ecommerce/uploads/".ler("SELECT uniq FROM ecommerce_prod_imagens WHERE id = {$valor['id_imagem_capa']}")[0]['uniq']."' height='100'/>";
